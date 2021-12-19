fe5036da7ed6ac67ebcfad6ddfbd67265017114e
Asiri Indatissa <asiri@unionassurance.com>
2021-12-19 13:02:11 +0530

    Add imperative approach

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 function App() {
-
+  // const para = document.createElement('p');
+  // para.textContent = "this is a paragrapgh";
+  // document.getElementById('root').append(para);
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>this is a paragrapgh</p>
     </div>
   );
 }
